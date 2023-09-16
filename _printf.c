2fb7c20e9333a703c389c15fb15e85d43a8a9677
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-16 04:43:26 +0300

    printf0_update

--- _printf.c
+++ _printf.c
@@ -11,20 +11,20 @@
  *
  *
 */
-int cases_int(unsigned int *i,const char *format, int num, unsigned int per);
-int cases_str(unsigned int *i,const char *format, char *s, unsigned int per);
-int cases_char(unsigned int *i,const char *format, char c, unsigned int per);
-int case_per(unsigned int *i,const char *format, unsigned int per);
+int cases_int(unsigned int i,const char *format, int num, unsigned int per);
+int cases_str(unsigned int i,const char *format, char *s, unsigned int per);
+int cases_char(unsigned int i,const char *format, char c, unsigned int per);
+int case_per(unsigned int i,const char *format, unsigned int per);
 
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0, per = 0;
 	va_list args;
-	char c;
+	char c = '?';
 	int num, n = 0;
 
-	if (!format || !format[0])
-		return (0);
+	if (!format)
+		return (-1);
 	va_start(args, format);
 
 	while (format[i])
@@ -33,24 +33,26 @@ int _printf(const char *format, ...)
 		{
 			case '%':
 				per++;
-				case_per(&i, format, per);
+				n += case_per(i, format, per);
 				break;
 
 			case 'c':
-				c = va_arg(args, int);
-				cases_char(&i, format, c, per);
+				c = (va_arg(args, int));
+				write(1, &c, 1);
+				_putchar(c);
+				n += cases_char(i, format, c, per);
 				per = 0;
 				break;
 
 			case 's':
-				cases_str(&i, format, va_arg(args, char *), per);
+				n += cases_str(i, format, va_arg(args, char *), per);
 				per = 0;
 				break;
 
 			case 'i':
 			case 'd':
 				num = va_arg(args, int);
-				cases_int(&i, format, num, per);
+				n += cases_int(i, format, num, per);
 				per = 0;
 				break;
 
@@ -64,11 +66,11 @@ int _printf(const char *format, ...)
 	return (n);
 }
 
-int case_per(unsigned int *i,const char *format, unsigned int per)
+int case_per(unsigned int i,const char *format, unsigned int per)
 {
 	char *str = "idsc";
 	int j = 0, next = 0, n = 0;
-	unsigned int k = *i;
+	unsigned int k = i;
 
 	while (str[j])
 	{
@@ -79,40 +81,43 @@ int case_per(unsigned int *i,const char *format, unsigned int per)
 	
 	if (per % 2 && next == 0)
 	{
-		n += _putchar(format[*i]);
+		n += _putchar(format[i]);
 	}
 	return (n);	
 }
 
-int cases_int(unsigned int *i,const char *format, int num, unsigned int per)
+int cases_int(unsigned int i,const char *format, int num, unsigned int per)
 {
 	int n = 0;
 
 	if (per % 2)
 		n += print_number(num);
 	else
-		n += _putchar(format[*i]);
+		n += _putchar(format[i]);
 	return (n);
 }
 
-int cases_str(unsigned int *i,const char *format, char *str, unsigned int per)
+int cases_str(unsigned int i,const char *format, char *str, unsigned int per)
 {
 	int n = 0;
 
 	if (per % 2)
 		n += _puts(str);
 	else
-		n += _putchar(format[*i]);
+		n += _putchar(format[i]);
 	return (n);
 }
 
-int cases_char(unsigned int *i,const char *format, char c, unsigned int per)
+int cases_char(unsigned int i,const char *format, char c, unsigned int per)
 {
 	int n = 0;
 
 	if (per % 2)
-		n += _putchar(c);
+	{
+		n += printf("%c",c);
+		_putchar('?');
+	}
 	else
-		n += _putchar(format[*i]);
+		n += _putchar(format[i]);
 	return (n);
 }
--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <limits.h>
 #include <string.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 int _putchar(char c);
 int _puts(char *str);
