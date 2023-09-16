65921fd99831407365f46804580d6cc5350ef185
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-16 09:36:43 +0300

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # printf Project README.md
+
+## this repo contains the following files
+
+1- _printf.c:
+    this file contains the function _printf that that produces output according to a format.
+
+2- func1.c
+    this file contains functions used to print different data types.
+
+3- func2.c
+    this file contains functions used to handle the identifiers such as (%,c,s,i,d).
+    
+4- main.h
+    header file.
--- test/main.c
+++ test/main.c
@@ -10,35 +10,21 @@
 int main(void)
 {
     int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+    int len2, n1 = -700;
+    char c = 'x', c2 = 'y', *c3 = "asbd";
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
+
+    len = _printf("sLet's try top%c printf%c a s%simple se%ddnten%%%%%i%ice.\n", c, c2, c3, n1,5,-5);
+    len2 = printf("sLet's try top%c printf%c a s%simple se%ddnten%%%%%i%ice.\n", c, c2, c3, n1,5,-5);
+    len2 = printf("sLet's t%ry toc printf%%%%\n");
+    len = _printf("sLet's t%ry toc printf%%%%\n");
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
+ 
+    printf("%d\n%d\n", len2, len);
+
+
     return (0);
 }
