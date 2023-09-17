feb22bee180c8220a14121fc765e3f0f06455998
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2023-09-17 21:42:23 +0300

    Special characters

--- _printf2/main.c
+++ _printf2/main.c
@@ -16,21 +16,13 @@ int main(void)
     char c0 = '\n';
     char *s = "";*/
 
-    int x = 0;
+    int x = 1;
     len2 = printf("%]\n");
     len = _printf("%]\n");
-    while(x < 1)
+    while(x < 32)
     {
-        _printf("bin:[%u, %i]\n", 89596, len2);
-        printf("bin:[%u, %i]\n", 89596, len);
-        _printf("oct:[%o, %o]\n", 89596, len2);
-        printf("oct:[%o, %o]\n", 89596, len);
-        _printf("hex:[%x, %x]\n", 89596, len2);
-        printf("hex:[%x, %x]\n", 89596, len);
-        _printf("HEX:[%X, %X]\n", 89596, len2);
-        printf("HEX:[%X, %X]\n", 89596, len);
-        _printf("HEX:[%S, %X]\n", "hama\nda\t", len2);
-        printf("HEX:[%S, %X]\n", "hama\nda\t", len);
+        _printf("HEX:[%S, %X]\n", x, len2);
+        printf("HEX:[%S, %X]\n", x, len);
         
         x++;
     }
